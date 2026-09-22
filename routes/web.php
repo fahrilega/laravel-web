2b9eede5bdc2bae5a77818c2d812cc7a0db1f7d4
Fahri <fahri25si@mahasiswa.pcr.ac.id>
2026-09-22 08:32:51 +0700

    comiit baru

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/fahri', function () {
+    return 'Hobi saya mancing';
+});
+
+Route::get('/fahri', function() {
+    return 'Halo Fikri';
+});
+
+Route::get('{param1}/{param2}/fahri', function() {
+    return 'Halo fahri';
+});
+
+Route::get('/{param1}/nama/', function ($param1) {
+    if($param1 == 'fahri')
+        return 'lega';
+    else
+        return 'Nama saya: '.$param1;
+});
